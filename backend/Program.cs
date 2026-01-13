389ec0d8a02bf25a53c1b899b9da7916dab48e51
higa <nikitagroshev11@gmail.com>
2026-01-13 18:34:31 +0300

    Added ready checkerr

--- backend/Program.cs
+++ backend/Program.cs
@@ -7,3 +7,14 @@ string? name = Console.ReadLine();
 Console.WriteLine($"Привет {name}!");
 Console.WriteLine(DateTime.Now.DayOfWeek);
 
+Console.WriteLine("Хотите продолжить? (y/n)");
+string? ready = Console.ReadLine();
+if (ready == "y")
+{
+    Console.WriteLine("You ready.");
+}
+else
+{
+    Console.WriteLine("Exit...");
+}
+
